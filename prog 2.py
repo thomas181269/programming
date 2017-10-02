c67554de6c6bd271e522cd8f5390423e492ef5be
thomas181269 <32454967+thomas181269@users.noreply.github.com>
2017-10-02 13:53:40 +0100

    Add files via upload

---
+++ prog 2.py
@@ -0,0 +1,2 @@
+print("Hello world")
+print("i am in my ISD class right now")
---
+++ prog3.py
@@ -0,0 +1,9 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("Hello world")
+Hello world
+>>> 
+==================== RESTART: C:/Users/21345145/prog 2.py ====================
+Hello world
+i am in my ISD class right now
+>>> 
